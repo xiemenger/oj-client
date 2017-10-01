3051e5a9356c1475cc4edf52a1c1d16d64ad07c5
Diane Lin <[tbaby.menger@gmail.com]>
2017-09-30 22:39:42 -0400

    created new-problem, nav-bar component

--- oj-client/src/app/app.module.ts
+++ oj-client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { routing } from "./app.routes";
 import { AppComponent } from './app.component';
@@ -8,16 +9,26 @@ import { ProblemDetailComponent } from './Components/problem-detail/problem-deta
 
 
 import { DataService } from "./services/data.service";
+import { NewProblemComponent } from './Components/new-problem/new-problem.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProblemListComponent,
-    ProblemDetailComponent
+    ProblemDetailComponent,
+    NewProblemComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule,
+    BrowserAnimationsModule,
+    MdButtonModule, 
+    MdCheckboxModule
   ],
   providers: [{
     provide: "data",
@@ -26,3 +37,4 @@ import { DataService } from "./services/data.service";
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
